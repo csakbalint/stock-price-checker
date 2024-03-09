7c1ed926202491f143885c48063a33c566c6744f
Balint Csak <csak.balint@allwin.hu>
2024-03-09 13:27:47 +0100

    chore: added lint-staged, husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,13 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: [
+    'prettier',
+    '@typescript-eslint/eslint-plugin',
+    'simple-import-sort',
+  ],
   extends: [
+    'prettier',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
@@ -17,9 +22,42 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    'prettier/prettier': 'error',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'import/prefer-default-export': 'off',
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': [
+      'warn',
+      {
+        groups: [
+          // Node.js builtins. You could also generate this regex if you use a `.js` config.
+          // For example: `^(${require("module").builtinModules.join("|")})(/|$)`
+          // external packages. own external libraries last
+          [
+            '^(assert|buffer|child_process|cluster|console|constants|crypto|dgram|dns|domain|events|fs|http|https|module|net|os|path|punycode|querystring|readline|repl|stream|string_decoder|sys|timers|tls|tty|url|util|vm|zlib|freelist|v8|process|async_hooks|http2|perf_hooks)(/.*|$)',
+            '^[^.]',
+          ],
+          ['^@common'],
+          // Internal packages.
+          ['^src'],
+          // Side effect imports.
+          ['^\\u0000'],
+          // Parent imports. Put `..` last.
+          ['^\\.\\./?$', '^\\.\\.(?!/?$)'],
+          // Other relative imports.
+          ['^\\./(?=.*/)(?!/?$)'],
+          // Put same-folder imports and `.` last.
+          ['^\\.(?!/?$)', '^\\./?$'],
+          // Style imports.
+          ['^.+\\.s?css$'],
+        ],
+      },
+    ],
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'sort-imports': 'off',
+    'import/order': 'off',
   },
 };
